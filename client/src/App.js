a2123047654d41b8ce7fcf97147bd8e4362f6a2e
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-08-22 20:53:56 -0400

    CREATE(now playing): Set up button to get now playing

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,61 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import SpotifyWebApi from "spotify-web-api-js";
+const spotifyApi = new SpotifyWebApi();
 
 class App extends Component {
+  constructor() {
+    super();
+    const params = this.getHashParams();
+    const token = params.access_token;
+    if (token) {
+      spotifyApi.setAccessToken(token);
+    }
+    this.state = {
+      loggedIn: token ? true : false,
+      nowPlaying: { name: "Not Checked", albumArt: "" }
+    };
+  }
+
+  getNowPlaying() {
+    spotifyApi.getMyCurrentPlaybackState().then(response => {
+      console.log(response);
+      if(response){
+        this.setState({
+          nowPlaying: {
+            name: response.item.name,
+            albumArt: response.item.album.images[0].url
+          }
+        });
+      }
+    });
+  }
+
+  getHashParams() {
+    var hashParams = {};
+    var e,
+      r = /([^&;=]+)=?([^&;]*)/g,
+      q = window.location.hash.substring(1);
+    e = r.exec(q);
+    while (e) {
+      hashParams[e[1]] = decodeURIComponent(e[2]);
+      e = r.exec(q);
+    }
+    return hashParams;
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <a href="http://localhost:8888"> Login to Spotify </a>
+        <div>Now Playing: {this.state.nowPlaying.name}</div>
+        <div>
+          <img src={this.state.nowPlaying.albumArt} style={{ height: 150 }} />
+        </div>
+        {this.state.loggedIn && (
+          <button onClick={() => this.getNowPlaying()}>
+            Check Now Playing
+          </button>
+        )}
       </div>
     );
   }
